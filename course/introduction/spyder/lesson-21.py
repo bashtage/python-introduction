22c91f49542f695793ca0b0572c876ed06b9b21e
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2019-10-21 18:34:15 +0100

    Ad missing chapters
    Former-commit-id: 8e4e7581adc9823747ee9c16ebfa9943a939727a

---
+++ course/introduction/spyder/lesson-21.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#%%
+# # Graphics: Line Plots
+# 
+# This lesson covers:
+# 
+# * Basic plotting 
+# * Subplots 
+# * Histograms 
+# * Scatter Plots
+
+#%%
+# Plotting in notebooks requires using a magic command, which starts with
+# `%`, to initialize the plotting backend.
+
+#%%
+# Setup
+from IPython import get_ipython
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+#%%
+# Begin by loading the data in hf.h5. This data set contains high-frequency
+# price data for IBM and MSFT on a single day stored as two Series. IBM is
+# stored as "IBM" in the HDF file, and MSFT is stored as "MSFT.
+
+#%%
+
+
+#%%
+# ## Problem: Basic Plotting
+# 
+# 1. Plot the `ibm` series which contains the price of IBM. 
+# 2. Add a title and label the axes. 
+# 3. Add markers and remove the line. 
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+# ## Problem: Subplot
+# 
+# Create a 2 by 1 subplot with the price of IBM in the top subplot and the
+# price of MSFT in the bottom subplot. 
+
+#%%
+
+
+#%%
+# ## Problem: Plot with Dates
+# 
+# Use `matplotlib` to directly plot `ibm` against its `index`. This is a
+# repeat of a previous plot but shows how to use the `plot` command directly. 
+
+#%%
+
+
+#%%
+# ## Exercises
+# 
+# ### Exercise: Change seaborn
+# 
+# Produce a line plot of MSFT's price using seaborn's "whitegrid" style.
+
+#%%
+
+
+#%%
+# ### Exercise: HLOC plot
+# 
+# Use the HLOC data to produce a plot of MSFT's 5 minute HLOC
+# where the there are no lines, high is demarcated using a green triangle,
+# low is demarcated using a red downward pointing triangle, open is demarcated 
+# using a light grey leftward facing triangle and close is demarcated using
+# a right facing triangle.
+# 
+# **Note** Get the axes from the first, plot, and reuse this when plotting 
+# the other series.
+
+#%%
+# Setup: Load data and create values
+import pandas as pd
+
+msft = pd.read_hdf("data/hf.h5", "MSFT")
+msft_5min = msft.resample("300S")
+high = msft_5min.max()
+low = msft_5min.min()
+open = msft_5min.first()
+close = msft_5min.last()
+
+
+#%%
+
---
+++ course/introduction/spyder/lesson-22.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#%%
+# # Graphics: Other Plots
+# 
+# This lesson covers:
+# 
+# * Histograms 
+# * Scatter Plots
+
+#%%
+# Plotting in notebooks requires using a magic command, which starts with `%`,
+# to initialize the plotting backend.
+
+#%%
+# Setup
+from IPython import get_ipython
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+#%%
+# Begin by loading the data in hf.h5. This data set contains high-frequency
+# price data for IBM and MSFT on a single day stored as two Series. IBM is
+# stored as "IBM" in the HDF file, and MSFT is stored as "MSFT.
+
+#%%
+
+
+#%%
+# ## Problem: Histogram
+# 
+# Produce a histogram of MSFT 1-minute returns (Hint: you have to produce
+# the 1-minute Microsoft returns first using `resample` and `pct_change`).
+
+#%%
+
+
+#%%
+# ## Problem: Scatter Plot
+# 
+# Scatter the 5-minute MSFT returns against the 5-minute IBM returns.
+# 
+# *Hint*: You will need to create both 5-minute return series, merge them,
+# and then plot using the combined DataFrame. 
+
+#%%
+
+
+#%%
+# ## Problem: Saving plots
+# 
+# Save the previous plot to PNG and PDF.
+
+#%%
+
+
+#%%
+# ## Exercises
+# 
+# ### Exercise: Visualize 5 and 10 minute returns
+# 
+# Produce a 2 by 1 subplot with a histogram of the 5-minute returns of IBM in the
+# top panel and 10-minute returns of IBM in the bottom. Set an appropriate title on
+# each of the 2 plots.
+
+#%%
+
+
+#%%
+# ### Exercise: Export the result of the previous exercise to JPEG and PDF   
+# 
+
+#%%
+
+
+#%%
+# ## Exercise: Plot histograms and a scatter plot
+# 
+# Produce a 2 by 2 subplot with:
+# 
+# * Set the figure size to be 10 by 10
+# * Histograms of IBM and MSFT on the diagonals
+# * Scatter plots on the off-diagonals where the x and y line up with the
+#   histogram on the diagonal.
+# * Set the limits of the scatter plots to match the appropriate histogram
+#   x and y limit.
+# * Clean up the plot using `tight_layout`
+
+#%%
+
+
+#%%
+# ### Exercise: Use pandas plotting tools
+# 
+# Use `pandas.plotting.scatter_matrix` to produce a similar plot to the previous exercise.
+
+#%%
+
